cf289639d2a01fe4e1a8aa5ea7423ef7c36955e5
pancho404 <franciscosanchez2101@gmail.com>
2021-10-05 16:19:29 -0300

    Updated button.h

--- FlappyBird/button.h
+++ FlappyBird/button.h
@@ -1,8 +1,24 @@
 #pragma once
-class button
+#include <iostream>
+#include "raylib.h"
+
+class Button
 {
 private:
-	
+	Rectangle buttonRec;
+	Color buttonColor;
+	Color secondaryColor;
 public:
+	Button(float width, float height, int x, int y, Color buttonColor, Color secondaryColor);
+	~Button();
+	Rectangle getButtonRec();	
+	float getButtonWidth();
+	float getButtonHeight();
+	int getButtonX();
+	int getButtonY();
+	Color getButtonColor();
+	void drawButton();
+	bool isMouseOnButton();
+	bool isButtonPressed();	
 };
 
